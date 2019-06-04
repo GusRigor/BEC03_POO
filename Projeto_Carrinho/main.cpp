901f5b424d6042c3c884985f27d2a251c0ae702c
Gustavo Rigor <37718815+GusRigor@users.noreply.github.com>
2019-06-04 19:12:20 -0300

    Update main.cpp

--- Projeto_Carrinho/main.cpp
+++ Projeto_Carrinho/main.cpp
@@ -1,34 +1,25 @@
 #include <iostream>
 #include <malloc.h>
-#ifndef SM_H_INCLUDED
-#define SM_H_INCLUDED
 #define PI 3.14159265
 #include <math.h>
 
-class SM{
+class ME{
   protected:
   float grau;
   float distancia;
 
   public:
-  SM(){
+  ME(){
     distancia=0.0;
     grau = 90.0;
   }
 
   void step(float iniciox, float fimx, float inicioy, float fimy, float giro, int posicao, float **saida){
-        //std::cout<<"Step posição "<<((posicao)/2)<<std::endl;
     proximoValor(iniciox, fimx, inicioy, fimy, giro, posicao, saida);
   }
 
   void transduce(float* entradas,int tamanho,float **saidas){
-  	//std::cout<<"I transduce tamanho"<<tamanho<<std::endl;
     for(int i=0, j=0; i <=(tamanho/2)-2; i=i+2,j++){
-        //std::cout<<"I transduce "<<i<<std::endl;
-        //std::cout<<"Entrada i  "<<entradas[i]<<std::endl;
-        //std::cout<<"Entrada 2+i  "<<entradas[2+i]<<std::endl;
-        //std::cout<<"Entrada 1+i  "<<entradas[1+i]<<std::endl;
-        //std::cout<<"Entrada 3+i  "<<entradas[3+i]<<std::endl;
         if((i+2)<=tamanho){
             step(entradas[i], entradas[2+i], entradas[1+i], entradas[3+i], grau, j, saidas);
 
@@ -39,7 +30,7 @@ class SM{
     }
   }
   float Posicao(){
-      //return (x,y);
+
   return 0;
   }
 
@@ -49,7 +40,7 @@ class SM{
 };
 
 
-class Anda:public SM{
+class Anda:public ME{
 protected:
     float distancia;
     float xck;
@@ -63,24 +54,20 @@ public:
         yck=0.0;
     }
     float proximoValor(float iniciox, float fimx, float inicioy, float fimy, float giro, int posicao, float **saida){
-        //std::cout<<"PASSOU prox valor anda"<<std::endl;
-        std::cout<<"POSICAO anda "<<(posicao)<<std::endl;
+        //std::cout<<"POSICAO anda "<<(posicao)<<std::endl;
         distancia = sqrt(pow((fimx-iniciox),2) + pow((fimy-inicioy),2));
-        //std::cout<<"Posicao : "<<(posicao)/2<<std::endl;
         for (int i=1, j=2, k=1; i<=20; i=i+2, j=j+2, k++){
             xck=((fimx-iniciox)/10)*(k)+iniciox;
             yck=((fimy-inicioy)/10)*(k)+inicioy;
             saida[posicao][i] = xck;
             saida[posicao][j] = yck;
-            //std::cout<<"Anda xck: "<<xck<<"  yck: "<<yck<<std::endl;
-            std::cout<<"Anda xck: "<<saida[posicao][i]<<" i: "<<i<<"  yck: "<<saida[posicao][j]<<" j: "<<j<<std::endl;
-            //std::cout<<"distancia "<<distancia<<std::endl;
+          //  std::cout<<"Anda xck: "<<saida[posicao][i]<<" i: "<<i<<"  yck: "<<saida[posicao][j]<<" j: "<<j<<std::endl;
         }
         return distancia;
     }
 };
 
-class Giro:public SM{
+class Giro:public ME{
 private:
     float atanx;
 protected:
@@ -91,12 +78,7 @@ public:
         giroAtual = 0.0;
     }
     float proximoValor(float iniciox, float fimx, float inicioy, float fimy, float giro, int posicao, float **saida){
-        //std::cout<<"PASSOU prox valor giro"<<std::endl;
-        //std::cout<<"posicao prox valor giro "<<((posicao)/2)<<std::endl;
         atanx = (float)atan(sqrt(pow((fimy-inicioy),2))/sqrt(pow((fimx-iniciox),2))) * 180 / PI;
-        //atanx = atan((fimy-inicioy)/(fimx-iniciox))* 180 / PI;
-        //std::cout<<"giroatual "<<giroAtual<<std::endl;
-        //std::cout<<"atanx "<<atanx<<std::endl;
         if (posicao == 0){
             giroAtual = grau;
         }
@@ -104,7 +86,6 @@ public:
             if(fimy < inicioy){
                 giroAtual = 180.0;
             }else{
-                //std::cout<<"X= Y<: "<<grau<<std::endl;
                 giroAtual = 0.0;
             }
         }
@@ -118,41 +99,35 @@ public:
 
         if(fimx > iniciox){
             if(fimy < inicioy){
-                //std::cout<<"X> Y<: "<<grau<<std::endl;
                 giroAtual = grau + atanx;
                 grau = grau + giroAtual;
             }
             else{
-                //std::cout<<"X> Y>: "<<grau<<std::endl;
                 giroAtual = grau - (180.0 - atanx);
                 grau = 90.0 + atanx;
             }
         }
         else{
             if (fimy < inicioy){
-                //std::cout<<"X< Y<: "<<grau<<std::endl;
                 giroAtual =  grau - (270.0 - atanx);
                 grau = 180 - atanx;
             }
             else{
-                //std::cout<<"X< Y>: "<<grau<<std::endl;
                 giroAtual = grau - atanx;
                 grau = atanx;
             }
         }
-        //std::cout<<"posicao giro fim: "<<((posicao)/2)<<std::endl;
+
         saida[(posicao)][0] = giroAtual;
-        //std::cout<<"GIROATUAL: "<<giroAtual<<std::endl;
-        //std::cout<<"GRAU: "<<grau<<std::endl;
-        //std::cout<<" "<<std::endl;
+
         return grau;
   }
 };
 
-class Carro:public SM{
+class Carro:public ME{
 private:
-    SM *gira;
-    SM *ando;
+    ME  *gira;
+    ME  *ando;
 protected:
     float distancia;
 
@@ -162,8 +137,7 @@ public:
         ando = new Anda();
     };
     float proximoValor(float iniciox, float fimx, float inicioy, float fimy, float giro, int posicao, float **saida){
-        //std::cout<<"PASSOU prox valor carro"<<std::endl;
-        //std::cout<<"posicao prox valor carro "<<((posicao)/2)<<std::endl;
+
         gira->proximoValor(iniciox, fimx, inicioy, fimy, giro, posicao, saida);
         distancia = ando->proximoValor(iniciox, fimx, inicioy, fimy, giro, posicao, saida);
         return distancia;
@@ -171,7 +145,7 @@ public:
 };
 
 
-#endif // SM_H_INCLUDED
+
 
 
 using namespace std;
@@ -183,20 +157,20 @@ int main()
     float entradas[22]={5.0,5.0,7.0,2.0,9.0,5.0,2.0,10.0,1.0,1.0,5.0,5.0,7.0,2.0,9.0,5.0,2.0,10.0,1.0,1.0,5.0,5.0};
     float **saida;
     saida=(float **)malloc((tamanho-2)*sizeof(float*));
-    //cout<<"tamanho calculado "<<((tamanho/2)+1)<<endl;
+
     for(int i=0; i<((tamanho/2)+2); i++){
         saida[i]=(float*)malloc((tamanho-2)*sizeof(float));
     };
 
-    Carro carrox;
+    Carro relampagoMarquinhos;
 
-    carrox.transduce(entradas,tamanho,saida);
+    relampagoMarquinhos.transduce(entradas,tamanho,saida);
 
     for(int i=0; i<(tamanho/4);i++){
-            //cout<<"iiiiiiii "<<i<<endl;
-            cout<<"saida i "<<i<<" angulo "<<" "<<saida[i][0]<<endl;
-        for(int j=1, k=2; j<=20;j=j+2, k=k+2){
-            cout<<" x "<<" "<<saida[i][j]<<" y "<<saida[i][k]<<endl;
+            cout<<endl;
+            cout<<"Ponto: "<<(i+1)<<" & Ângulo para rotação: "<<saida[i][0]<<"º."<<endl;
+        for(int j=1, k=2, l=1; j<=20;j=j+2, k=k+2, l++){
+            cout<<"Tempo "<<l<<": ("<<saida[i][j]<<", "<<saida[i][k]<<");"<<endl;
         }
     }
     return 0;
